8045f20bca5d2e4736d2956b485cfcbf7e43f608
lil-sahil <sahilnagpal93@yahoo.ca>
2022-07-04 15:39:38 -0400

    Added cors middleware.

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 connectDB();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 
 app.use("/api/yearly_data", require("./routes/yearRoutes"));
 app.use("/api/", require("./routes/playerRoutes"));
-
 app.listen(port, () => console.log(`Server started on port ${port}`));
