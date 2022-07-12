88577682bfd26eca2a1988580fe95f38f54e1f6f
lil-sahil <sahilnagpal93@yahoo.ca>
2022-07-12 16:20:19 -0400

    Allow to compare against name accents.

--- backend/controllers/playerController.js
+++ backend/controllers/playerController.js
@@ -1,23 +1,35 @@
 const yearlyModel = require("../models/yearlyModels");
 
+// Utils
+const { removeAccents } = require("../utils/stringManipulation");
+
 // @desc    GET Player Data
 // @route   GET /api/:playerData
 // @access  Public
 
 const getPlayerData = async (req, res) => {
   let playerData = [];
+
+  // If the user enters a name with an accent the remove accent function will sanitize it.
+
   for (let year = 1980; year <= 2022; year++) {
     let results = {
       Year: year,
-      Data: await yearlyModel(year.toString()).find({
-        player: { $regex: req.params.playerName, $options: "i" },
-      }),
+      Data: await yearlyModel(year.toString())
+        .find({
+          player: { $regex: req.params.playerName, $options: "i" },
+        })
+        .collation({ locale: "en", strength: 1 }),
     };
 
     Object.keys(results.Data).length > 0 ? playerData.push(results) : 1;
   }
 
-  res.status(200).json(playerData);
+  if (playerData.length === 0) {
+    res.status(400).json([]);
+  } else {
+    res.status(200).json(playerData);
+  }
 };
 
 module.exports = {
