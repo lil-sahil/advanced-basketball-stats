0a52158c8353f7570bdb93d085b510f7ed49f0bc
lil-sahil <sahilnagpal93@yahoo.ca>
2022-07-13 17:00:07 -0400

    Click handler function updated to handle accents in names, as well as recomend players found based on user entry.

--- frontend/src/components/SearchFields.js
+++ frontend/src/components/SearchFields.js
@@ -21,7 +21,25 @@ const SearchFields = (props) => {
     });
     let data = await response.json();
 
-    return data;
+    if (uniqueNames(data) === 1) {
+      props.setPlayerData(data);
+      return 1;
+    }
+
+    // More than one player found
+    else if (uniqueNames(data) > 1) {
+      let playerNamesFound = data.map((item) => {
+        return item.Data[0].player;
+      });
+
+      console.log(playerNamesFound);
+
+      // Set state with the list of player names found and have the use select who they are looking for.
+
+      return 1;
+    }
+
+    return 0;
   };
 
   const uniqueNames = (arr) => {
@@ -37,41 +55,36 @@ const SearchFields = (props) => {
 
     e.preventDefault();
 
-    let data = await fetchData(props.playerName);
-
-    // let response = await fetch(
-    //   `http://localhost:5000/api/${props.playerName}`,
-    //   {
-    //     mode: "cors",
-    //   }
-    // );
-    // let data = await response.json();
-
-    if (uniqueNames(data) === 1) {
-      props.setPlayerData(data);
-    } else {
-      let data = fetchData(props.playerName.split(" ")[0]);
-      let playerNamesFound = data.map((item) => {
-        return item.Data[0].player;
-      });
-
-      console.log(playerNamesFound);
+    if (await fetchData(props.playerName)) {
+      return 1;
     }
 
-    // if (data.length === 0) {
-    //   props.setResponse("Not valid");
+    // No names found
+    else {
+      // remove accent from name and search again
+      let name = removeAccents(props.playerName);
+      let firstName = name.split(" ")[0];
 
-    //   // Search by first name only and suggest players
-    //   let data = fetchData(props.playerName.split(" ")[0]);
-    //   let playerNamesFound = data.map((item) => {
-    //     return item.Data[0].player;
-    //   });
+      if (await fetchData(firstName)) {
+        return 1;
+      } else {
+        for (
+          let letterIndex = firstName.length - 1;
+          letterIndex > 0;
+          letterIndex--
+        ) {
+          let name = firstName.slice(0, letterIndex);
+          if (await fetchData(name)) {
+            return 1;
+          }
+        }
 
-    //   console.log(playerNamesFound);
-    // }
+        props.setResponse("Not valid");
+      }
+    }
   };
 
-  // Useeffect hook will run on initial render to show the data for the mp_per_g
+  // UseEffect hook will run on initial render to show the data for the mp_per_g
   useEffect(() => {
     props.setStatSelection("mp_per_g");
   }, []);
