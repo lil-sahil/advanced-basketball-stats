08f7d57855d48abc069674b0ece4c80389de74b7
lil-sahil <sahilnagpal93@yahoo.ca>
2022-07-25 21:45:35 -0400

    Added icons and styling to SidePlayerBar components. Used the playerId to fetch and display the team image. Also added stats icon to Title component.

--- frontend/src/components/SidePlayerBar.js
+++ frontend/src/components/SidePlayerBar.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Stats from "./Stats";
 
+// Icons
+import { FaWeightHanging } from "react-icons/fa";
+import { GiBodyHeight } from "react-icons/gi";
+
+// Images
+import logoman from "../assets/logoman.png";
+
 // Custom Hooks
 import { useFetchPlayerId } from "../hooks/useFetchPlayerId";
 import { useFetchPlayerGeneralStats } from "../hooks/useFetchPlayerGeneralStats";
@@ -8,19 +15,55 @@ import { useFetchPlayerGeneralStats } from "../hooks/useFetchPlayerGeneralStats"
 const SidePlayerBar = (props) => {
   let playerId = useFetchPlayerId([props.playerData], props.playerName);
 
-  let { points, assists, rebounds, position, height, weight } =
-    useFetchPlayerGeneralStats([props.playerData], props.playerName);
+  let {
+    points,
+    assists,
+    rebounds,
+    position,
+    height,
+    weight,
+    teamId,
+    teamName,
+    jerseyNumber,
+  } = useFetchPlayerGeneralStats([props.playerData], props.playerName);
 
   let successPlayerIdEle = (
     <>
       <div>
         <img
           src={`https://cdn.nba.com/headshots/nba/latest/1040x760/${playerId}.png`}
+          onError={({ currentTarget }) => {
+            currentTarget.onerror = null; // prevents looping
+            currentTarget.src = logoman;
+          }}
         ></img>
       </div>
 
-      <div className="font-sidebar-player-name mt-5">
-        {props.playerData[0]?.Data[0]?.player}
+      <div className="mt-2 mb-6">
+        <div>{`${teamName} | #${jerseyNumber} | ${position}`}</div>
+        <div className="font-sidebar-player-name mt-1 text-3xl text-[#F88158]">
+          {props.playerData[0]?.Data[0]?.player}
+        </div>
+      </div>
+
+      <div className="flex flex-row w-full justify-around mb-6">
+        <div className="text-xl">
+          <div className="flex mb-4">
+            <GiBodyHeight className="mr-2"></GiBodyHeight>
+            <>{height}</>
+          </div>
+          <div className="flex">
+            <FaWeightHanging className="mr-2"></FaWeightHanging>
+            <>{`${weight} lbs`}</>
+          </div>
+        </div>
+
+        <div className="h-full">
+          <img
+            src={`https://cdn.nba.com/logos/nba/${teamId}/global/D/logo.svg`}
+            className="h-full"
+          ></img>
+        </div>
       </div>
 
       <div className="flex items-center justify-between w-full px-5 mt-5">
@@ -34,7 +77,7 @@ const SidePlayerBar = (props) => {
   let nonSuccessPlayerIdEle = <></>;
 
   return (
-    <div className="order-first flex-shrink-0 w-96 mx-2 my-2 py-6 px-4 border-2 flex flex-col items-center justify-between">
+    <div className="order-first flex-shrink-0 w-96 mx-2 my-2 py-6 px-4 border-2 rounded-2xl flex flex-col items-center justify-between text-center">
       {playerId === "" ? nonSuccessPlayerIdEle : successPlayerIdEle}
     </div>
   );
--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+// Icons
+import { ImStatsDots } from "react-icons/im";
+
 const Title = () => {
-  return <div className="text-8xl mb-16 px-8 border-b-2">Hoop Stats</div>;
+  return (
+    <div className="flex mb-16 px-8 border-b-2">
+      <div className="text-8xl">Hoop Stats</div>
+      <ImStatsDots className="text-4xl self-end ml-6 mb-2 text-[#F88158]"></ImStatsDots>
+    </div>
+  );
 };
 
 export default Title;
