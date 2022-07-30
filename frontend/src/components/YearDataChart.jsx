3fa6ac4e3b86cf54e1e87f20e3188240ea6306d0
lil-sahil <sahilnagpal93@yahoo.ca>
2022-07-30 13:44:37 -0400

    Show top player images for All years.

--- frontend/src/components/YearDataChart.jsx
+++ frontend/src/components/YearDataChart.jsx
@@ -41,7 +41,8 @@ import { years } from "../config/yearConfig";
 
     // Labels
     const labels = (() => {
-        return props.yearSelection === "All" ? years.filter(item => item != "All") : props.data.map(item => item.Data.Player) 
+      console.log(props.data)
+        return props.yearSelection === "All" ? years.filter(item => item !== "All") : props.data.map(item => item.player) 
     })() 
     
     
@@ -74,6 +75,29 @@ import { years } from "../config/yearConfig";
     
 
     let [graphData, setGraphData] = useState([]);
+    let [topPlayerImages, setTopPlayerImages] = useState([]);
+
+    const getTopPlayersImages = async (labels) => {
+      let images = await Promise.all(
+        labels.map(async (year, index) => {
+          let playerData = await fetchGeneralData(graphData[0][index].player);
+          let playerId = await getPlayerId(playerData);
+  
+          let img = new Image(54.74, 40);
+  
+          img.src = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${playerId}.png`;
+          
+  
+          img.onerror = () => {
+            img.src = require("../assets/logoman.png");
+          };
+  
+          return img;
+        })
+      );
+      return images;
+    };
+
 
     useEffect(() => {
         let getData = async (arrayOfPercentiles) => {
@@ -92,34 +116,55 @@ import { years } from "../config/yearConfig";
           }
           setGraphData(percentileDataArray);
         };
-        getData([100]);
+        getData([100, 50]);
       }, [props.statSelection, props.yearData]);
+
+
+      function sleep(ms) {
+        return new Promise((resolveFunc) => setTimeout(resolveFunc, ms));
+      }
+
+      useEffect(() => {
+        const updateImages = async () => {
+          setTopPlayerImages([]);
+          let images = await getTopPlayersImages(labels);
+          await sleep(2000);
+          setTopPlayerImages(images);
+        };
+        if (graphData[0]) {
+          updateImages();
+        }
+      }, [graphData]);
 
   
   
     const data = {
       labels,
       datasets: [
-        // {
-        //   label: "Dataset 1",
-        //   data: props.data.map((item) =>
-            
-        //     parseFloat(item[props.statSelection])
-        //   ),
-        //   borderColor: "rgb(255, 99, 132)",
-        //   backgroundColor: "rgba(255, 99, 132, 0.5)",
-        // }
         {
-          fill: true,
-          backgroundColor: "rgba(53, 162, 235, 0.5)",
-          label: "25th percentile",
-          data: graphData[0]?.map((item) => item[props.statSelection]),
+          label: "Dataset 1",
+          data: props.data.map((item) =>
+            
+            parseFloat(item[props.statSelection])
+          ),
+          borderColor: "rgb(255, 99, 132)",
+          backgroundColor: "rgba(255, 99, 132, 0.5)",
         },
-        // {
-        //   fill: "-1",
-        //   label: "50th percentile",
-        //   data: graphData[1]?.map((item) => item[props.statSelection]),
-        // },
+        {
+          fill: false,
+          backgroundColor: "rgba(53, 162, 235, 0.5)",
+          label: "Leaders",
+          data: graphData[0]?.map((item) => item[props.statSelection]),
+          pointStyle: topPlayerImages,
+
+        },
+        {
+          fill: false,
+          label: "50th percentile",
+          data: graphData[1]?.map((item) => item[props.statSelection]),
+          borderColor: "rgb(255, 99, 132)",
+          backgroundColor: "rgba(255, 99, 132, 0.5)"
+        },
         // {
         //   fill: "-1",
         //   backgroundColor: "rgba(53, 162, 235, 0.5)",
